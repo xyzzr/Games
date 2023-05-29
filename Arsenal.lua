43eb1e404767280b150bb664ae4b2e274608ebb7
bobowawahahahbobobwahahawoaowahahahahahahaiaiaiaiaiahbboboboboboboboaiaiiapampampamramamamamapapapammaamama <110189904+bobowawahahahbobobwahahawoaowabobwabo@users.noreply.github.com>
2023-05-29 13:11:50 +0300

    Update Arsenal.lua

--- Arsenal.lua
+++ Arsenal.lua
@@ -1,4 +1,1 @@
-
-local hoes = "https://gist.githubusercontent.com/"
-local host = "bobowawahahahbobobwahahawoaowabobwabo/3def0f582e6b0fd59640f64fc4adac1f/raw/"
-loadstring(game:HttpGet(hoes ..  host  .. "099bb372f911835ab5d9ca6cf533137d97d1d04c/bobowawahahahbobobwahahawoaowabobwabobobowawahahahbobobwahahawoaowabobwabo"))()
+-- deleted
